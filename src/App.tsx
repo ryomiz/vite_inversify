15c643235c6b2424abbd869a98d4e4031dc7381f
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2025-05-19 21:16:30 +0200

    :lipstick: Implement Dialog for adding task

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,35 @@ import {
 	CardFooter,
 	CardHeader,
 } from "@/components/ui/card.tsx";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog.tsx";
 
 function App() {
 	return (
 		<div className={"mx-auto max-w-3xl px-4 py-8"}>
-			<Button>Create Task</Button>
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button>Create Task</Button>
+				</DialogTrigger>
+				<DialogContent className="sm:max-w-[425px]">
+					<DialogHeader>
+						<DialogTitle>Edit profile</DialogTitle>
+						<DialogDescription>
+							Make changes to your profile here. Click save when you're done.
+						</DialogDescription>
+					</DialogHeader>
+					<DialogFooter>
+						<Button type="submit">Save changes</Button>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
 			<hr className={"mt-4"} />
 			<div className={"mt-4 grid grid-cols-2 gap-4"}>
 				<div className={"flex flex-col gap-4"}>
