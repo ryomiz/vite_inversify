3a773b4f93ccb0467de3f3afe1f85a1f42eb3e76
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2025-05-20 14:49:12 +0200

    :lipstick: Implement a modal that create a task

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,84 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from "@/components/ui/dialog.tsx";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from "@/components/ui/form.tsx";
+import { Input } from "@/components/ui/input.tsx";
+import { Textarea } from "@/components/ui/textarea.tsx";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 function App() {
+	const schema = z.object({
+		title: z.string().nonempty({ message: "This field is required" }),
+		description: z.string().nonempty({ message: "This field is required" }),
+	});
+	type FormInput = z.infer<typeof schema>;
+	const form = useForm<FormInput>({
+		defaultValues: {
+			title: "",
+			description: "",
+		},
+		resolver: zodResolver(schema),
+	});
+
 	return (
 		<div className={"mx-auto max-w-3xl px-4 py-8"}>
 			<Dialog>
 				<DialogTrigger asChild>
-					<Button>Create Task</Button>
+					<Button>Add Task</Button>
 				</DialogTrigger>
 				<DialogContent className="sm:max-w-[425px]">
-					<DialogHeader>
-						<DialogTitle>Edit profile</DialogTitle>
-						<DialogDescription>
-							Make changes to your profile here. Click save when you're done.
-						</DialogDescription>
-					</DialogHeader>
-					<DialogFooter>
-						<Button type="submit">Save changes</Button>
-					</DialogFooter>
+					<Form {...form}>
+						<DialogHeader>
+							<DialogTitle>Add Task</DialogTitle>
+						</DialogHeader>
+						<DialogDescription>Add title and description.</DialogDescription>
+						<FormField
+							control={form.control}
+							name={"title"}
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<Input placeholder={"title"} {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name={"description"}
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<Textarea placeholder={"description"} {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<DialogFooter>
+							<Button
+								type="submit"
+								onClick={form.handleSubmit(async (input) => {
+									try {
+										console.log(input);
+									} catch {
+										// do something
+									}
+								})}
+							>
+								Create
+							</Button>
+						</DialogFooter>
+					</Form>
 				</DialogContent>
 			</Dialog>
 			<hr className={"mt-4"} />
@@ -40,9 +100,10 @@ function App() {
 					{[0, 1, 2].map((num) => (
 						<Card key={num}>
 							<CardHeader>Title</CardHeader>
-							<CardContent>Content</CardContent>
-							<CardFooter className={"flex justify-end"}>
-								<Button>Complete</Button>
+							<CardContent>Description</CardContent>
+							<CardFooter className={"flex justify-between"}>
+								<Button variant={"outline"}>Edit</Button>
+								<Button>Done</Button>
 							</CardFooter>
 						</Card>
 					))}
@@ -50,7 +111,7 @@ function App() {
 				<div>
 					<Card>
 						<CardHeader>Title</CardHeader>
-						<CardContent>Content</CardContent>
+						<CardContent>Description</CardContent>
 					</Card>
 				</div>
 			</div>
